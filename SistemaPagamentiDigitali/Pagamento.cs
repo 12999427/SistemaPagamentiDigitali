715d76cb9b8a7af7c0e341fa9c515ec10908f1cf
12999427 <12999427@SISTEMI26>
2025-10-24 11:51:45 +0200

    Aggiungere i file di progetto.

---
+++ SistemaPagamentiDigitali/Pagamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPagamentiDigitali
+{
+    internal class Pagamento
+    {
+        private float saldo;
+        public float Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                saldo = value;
+            }
+        }
+
+        private float importo;
+        public float Importo
+        {
+            get
+            { return importo; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                importo = value;
+            }
+        }
+
+        public Pagamento (float saldo, float importo)
+        {
+            Saldo = saldo;
+            Importo = importo;
+        }
+
+        public abstract bool VerificaDisponibilita ()
+        {
+
+        }
+    }
+}
